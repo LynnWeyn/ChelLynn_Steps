bff617763f9718698e3c1ec3581772ccb347e82a
Lynn Weyn <WYNLYN004@myuct.ac.za>
2020-10-02 15:16:55 +0200

    added basic pedometer function with a textview that updates

--- app/src/main/java/com/example/chellynn_steps/MainActivity.java
+++ app/src/main/java/com/example/chellynn_steps/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.chellynn_steps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    TextView step_display;
+    SensorManager sensorManager;
+    boolean running = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        step_display = (TextView) findViewById(R.id.tv_steps);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(countSensor!=null){
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        }
+        else{
+            Toast.makeText(this, "ERROR: Sensor not found!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
+        //sensorManager.unregisterListener(this); //hardware stops detecting steps. we dont want that
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (running){ //only update display if app open
+            step_display.setText(String.valueOf(event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
