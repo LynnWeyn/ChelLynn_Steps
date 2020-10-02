c5506c6ae8e207869b33788516f72026ffc8ef15
Lynn Weyn <WYNLYN004@myuct.ac.za>
2020-10-02 13:31:04 +0200

    all the other stuff for initialising repo

---
+++ app/src/main/java/com/example/chellynn_steps/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.chellynn_steps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
