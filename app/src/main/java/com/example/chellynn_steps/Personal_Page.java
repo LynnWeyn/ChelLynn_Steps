48d7586ec2b545f63b4b0409bf3a62aacda6a140
Lynn Weyn <WYNLYN004@myuct.ac.za>
2020-10-02 23:09:28 +0200

    added better buttons to home screen and name/class options

--- app/src/main/java/com/example/chellynn_steps/Personal_Page.java
+++ app/src/main/java/com/example/chellynn_steps/Personal_Page.java
@@ -15,7 +15,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,9 +33,24 @@ public class Personal_Page extends AppCompatActivity implements SensorEventListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal__page);
 
-        FloatingActionButton btn = (FloatingActionButton) findViewById(R.id.toHistory);
+//        FloatingActionButton btn = (FloatingActionButton) findViewById(R.id.toHistory);
+//
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(Personal_Page.this, History_Page.class));
+//            }});
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.GradeClasses,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+     //   spinner.setOnItemClickListener(this);
+
+        //button from home to history page
+
+        Button historyButton = (Button) findViewById(R.id.toHistoryButton);
+        historyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Personal_Page.this, History_Page.class));
