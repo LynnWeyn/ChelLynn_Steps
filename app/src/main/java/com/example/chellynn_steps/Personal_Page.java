81edf21982e9c7e054d37ca4736308fa041bff31
Lynn Weyn <WYNLYN004@myuct.ac.za>
2020-10-03 08:18:44 +0200

    progress bar working now

--- app/src/main/java/com/example/chellynn_steps/Personal_Page.java
+++ app/src/main/java/com/example/chellynn_steps/Personal_Page.java
@@ -7,16 +7,11 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +21,7 @@ public class Personal_Page extends AppCompatActivity implements SensorEventListe
     TextView step_display;
     SensorManager sensorManager;
     boolean running = false;
+    ProgressBar progressBar;
 
 
     @Override
@@ -33,22 +29,15 @@ public class Personal_Page extends AppCompatActivity implements SensorEventListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal__page);
 
-//        FloatingActionButton btn = (FloatingActionButton) findViewById(R.id.toHistory);
-//
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(Personal_Page.this, History_Page.class));
-//            }});
-
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.GradeClasses,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-     //   spinner.setOnItemClickListener(this);
+
+        //progress bar:
+        progressBar = (ProgressBar)findViewById(R.id.personalProgressBar);
 
         //button from home to history page
-
         Button historyButton = (Button) findViewById(R.id.toHistoryButton);
         historyButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,8 +45,23 @@ public class Personal_Page extends AppCompatActivity implements SensorEventListe
                 startActivity(new Intent(Personal_Page.this, History_Page.class));
             }});
 
-        step_display = (TextView) findViewById(R.id.tv_steps);
+        //button from home to class page
+        Button classButton = (Button) findViewById(R.id.toClassButton);
+        classButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Personal_Page.this, History_Page.class)); //to change to Class_Page
+            }});
 
+        //button from home to school page
+        Button schoolButton = (Button) findViewById(R.id.toSchoolButton);
+        schoolButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Personal_Page.this, History_Page.class)); //to change to School_Page
+            }});
+
+        step_display = (TextView) findViewById(R.id.tv_steps);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
     }
 
@@ -80,14 +84,14 @@ public class Personal_Page extends AppCompatActivity implements SensorEventListe
         running = false;
         //sensorManager.unregisterListener(this); //hardware stops detecting steps. we dont want that
     }
-
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (running){ //only update display if app open
-            step_display.setText(String.valueOf(event.values[0]));
+            step_display.setText(String.valueOf(event.values[0])); //change text
+            int i = (int) ((event.values[0]/1000)*100); //should be /10 000 but then progress too small to test with
+            progressBar.setProgress(i); //update progress bar
         }
     }
-
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
